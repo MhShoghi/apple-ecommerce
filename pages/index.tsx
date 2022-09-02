3e0c1193659d167db6d00b24846d1453137d4ca4
Mohammad Hosein Shoghi <hoseinshoghi@gmail.com>
2022-09-02 16:31:27 +0430

    some style and add haedless ui for tabs and start work with API

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,6 @@
 import { Tab } from "@headlessui/react";
-import type { NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
-import { useState } from "react";
 import Header from "../components/Header";
 import Landing from "../components/Landing";
 
@@ -27,13 +25,28 @@ const Home: NextPage = () => {
           </h1>
 
           <Tab.Group>
-            <Tab.List className="flex justify-center text-white">
-              <Tab>Tab1</Tab>
-              <Tab>Tab2</Tab>
+            <Tab.List className="flex justify-center">
+              {/* {categories.map((category) => (
+                <Tab
+                  key={category._id}
+                  id={category._id}
+                  className={({ selected }) =>
+                    `whitespace-nowrap rounded-t-lg py-3 px-5 text-sm font-light outline-none md:py-4 md:px-6 md:text-base ${
+                      selected
+                        ? "borderGradient bg-[#35383C] text-white"
+                        : "border-b-2 border-[#35383C] text-[#747474]"
+                    }`
+                  }
+                >
+                  iphone
+                </Tab>
+              ))} */}
             </Tab.List>
-
-            <Tab.Panels className="mx-auto max-w-fit pt-10 pb-24 text-white sm:px-4">
-              <Tab.Panel>Pannel 1</Tab.Panel>
+            <Tab.Panels className="mx-auto max-w-fit pt-10 pb-24 sm:px-4">
+              {/* <Tab.Panel className="tabPanel">{showProducts(0)}</Tab.Panel>
+              <Tab.Panel className="tabPanel">{showProducts(1)}</Tab.Panel>
+              <Tab.Panel className="tabPanel">{showProducts(2)}</Tab.Panel>
+              <Tab.Panel className="tabPanel">{showProducts(3)}</Tab.Panel> */}
             </Tab.Panels>
           </Tab.Group>
         </div>
@@ -43,3 +56,12 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+// Backend Code
+export const getServerSideProps: GetServerSideProps = async () => {
+  // const categories = await fetchCategories();
+
+  return {
+    props: {},
+  };
+};
